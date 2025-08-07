d5992346e29f87fd657844791b0969bc969e9479
Robloxhacker3 <mua35351@gmail.com>
2025-08-08 02:31:18 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-@@ .. @@
- import { Collaborators } from '@/components/collaborators'
- import { ConsoleOutput } from '@/components/console-output'
- import { LoginDialog } from '@/components/login-dialog'
- import { ProjectHistoryDialog } from '@/components/project-history-dialog'
- import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
- import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
--import { useToast } from '@/hooks/use-toast'
-+import { useToast } from '@/hooks/use-toast'
- import { ThemeProvider } from '@/components/theme-provider'
- import { v4 as uuidv4 } from 'uuid';
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to Coding Playground</h1>
+      {/* Add your components here */}
+    </main>
+  )
+}

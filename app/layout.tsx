9abcd2cdc08e21d295192a988143f85d673f4ddc
Robloxhacker3 <mua35351@gmail.com>
2025-08-08 02:13:56 +0300

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,1 @@
-@@ .. @@
- import type { Metadata } from 'next'
- import { GeistSans } from 'geist/font/sans'
- import { GeistMono } from 'geist/font/mono'
-+import { Toaster } from '@/components/ui/toaster'
- import './globals.css'
-
-@@ .. @@
-       </head>
--      <body>{children}</body>
-+      <body>
-+        {children}
-+        <Toaster />
-+      </body>
-     </html>
-   )
- }
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
